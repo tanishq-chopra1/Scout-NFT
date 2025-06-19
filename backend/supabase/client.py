c230c3892ee6ea155f0bd216719c1c309991d11e
tanishq-chopra1 <tanishqtc1980@gmail.com>
2025-06-19 14:02:03 -0500

    GitIgnore Updates

--- backend/supabase/client.py
+++ backend/supabase/client.py
@@ -1,8 +1,11 @@
 # backend/supabase/client.py
 import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
-SUPABASE_URL: str = os.getenv("https://pcxrnjquyzyzdupyzfpm.supabase.co")
-SUPABASE_KEY: str = os.getenv("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjeHJuanF1eXp5emR1cHl6ZnBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAyNjI4OTYsImV4cCI6MjA2NTgzODg5Nn0.H77CkDcdhwVUKlfgx9m_zjfIYSYLWngNbMN-VfwzS9w")
+load_dotenv()
+
+SUPABASE_URL: str = os.getenv("SUPABASE_URL")
+SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
